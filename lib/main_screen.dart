6e22d5691af6931522174ff8c9371123636f008a
Mohamad Daffa Argakoesoemah <argakoesoemahmdaffa@gmail.com>
2021-06-17 15:40:36 +0700

    First Commit

---
+++ lib/main_screen.dart
@@ -0,0 +1,235 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:sistem_akademik/news.dart';
+
+var username = 'Mohamad Daffa Argakoesoemah';
+
+class BackgroundImage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('images/background.png'),
+          fit: BoxFit.cover,
+        )
+      ),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('images/background.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ), SingleChildScrollView(
+            child: SafeArea(
+              child:
+              Container(
+                  margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
+                  child: Column(
+                      children: <Widget>[
+                        SizedBox(height: 50.0),
+                        Text(
+                          'Selamat Datang, $username!',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey.shade800,
+                          ),
+                        ),
+                        SizedBox(height: 30.0),
+                        Container(
+                            padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20.0),
+                              color: Colors.cyan.shade50.withOpacity(0.5),
+                            ),
+                            child: Column(
+                                children: [
+                                  Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Column(
+                                          children: [
+                                            Icon(
+                                              Icons.account_circle,
+                                              size: 40.0,
+                                              color: Colors.blue,
+                                            ),
+                                            SizedBox(height: 8.0),
+                                            Text(
+                                                'Profil',
+                                                style: TextStyle(
+                                                  fontSize: 13.0,
+                                                  color: Colors.grey.shade800,
+                                                )
+                                            )
+                                          ],
+                                        ),
+                                        Column(
+                                          children: [
+                                            Icon(
+                                              Icons.monetization_on,
+                                              size: 40.0,
+                                              color: Colors.green,
+                                            ),
+                                            SizedBox(height: 8.0),
+                                            Text(
+                                                'Keuangan',
+                                                style: TextStyle(
+                                                  fontSize: 13.0,
+                                                  color: Colors.grey.shade800,
+                                                )
+                                            )
+                                          ],
+                                        ),
+                                        Column(
+                                            children: [
+                                              Icon(
+                                                Icons.menu_book,
+                                                size: 40.0,
+                                                color: Colors.orange.shade300,
+                                              ),
+                                              SizedBox(height: 8.0),
+                                              Text(
+                                                  'Kurikulum',
+                                                  style: TextStyle(
+                                                    fontSize: 13.0,
+                                                    color: Colors.grey.shade800,
+                                                  )
+                                              )
+                                            ]
+                                        ),
+                                        Column(
+                                            children: [
+                                              Icon(
+                                                Icons.date_range_outlined,
+                                                size: 40.0,
+                                                color: Colors.redAccent.shade400,
+                                              ),
+                                              SizedBox(height: 8.0),
+                                              Text(
+                                                  'Jadwal',
+                                                  style: TextStyle(
+                                                    fontSize: 13.0,
+                                                    color: Colors.grey.shade800,
+                                                  )
+                                              )
+                                            ]
+                                        )
+                                      ]
+                                  ),
+                                  SizedBox(height: 8.0),
+                                  Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Column(
+                                            children: [
+                                              Icon(
+                                                Icons.school,
+                                                size: 40.0,
+                                                color: Colors.blue.shade900,
+                                              ),
+                                              SizedBox(height: 8.0),
+                                              Text(
+                                                  'Wisuda',
+                                                  style: TextStyle(
+                                                    fontSize: 13.0,
+                                                    color: Colors.grey.shade800,
+                                                  )
+                                              )
+                                            ]
+                                        ),
+                                        Column(
+                                            children: [
+                                              Icon(
+                                                Icons.web_outlined,
+                                                size: 40.0,
+                                                color: Colors.orange.shade600,
+                                              ),
+                                              SizedBox(height: 8.0),
+                                              Text(
+                                                  'Rencana Studi',
+                                                  style: TextStyle(
+                                                    fontSize: 13.0,
+                                                    color: Colors.grey.shade800,
+                                                  )
+                                              )
+                                            ]
+                                        ),
+                                        Column(
+                                            children: [
+                                              Icon(
+                                                Icons.settings,
+                                                size: 40.0,
+                                                color: Colors.blueGrey.shade700,
+                                              ),
+                                              SizedBox(height: 8.0),
+                                              Text(
+                                                  'Pengaturan',
+                                                  style: TextStyle(
+                                                    fontSize: 13.0,
+                                                    color: Colors.grey.shade800,
+                                                  )
+                                              )
+                                            ]
+                                        )
+                                      ]
+                                  )
+                                ]
+                            )
+                        ),
+                        SizedBox(
+                            height: 15.0
+                        ),
+                        Text('Berita', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17.0),),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(8.0),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20.0),
+                            color: Colors.cyan.shade50.withOpacity(0.5),
+                          ),
+                            child: ListView.builder(
+                              shrinkWrap: true,
+                              physics: NeverScrollableScrollPhysics(),
+                              itemBuilder: (BuildContext context, int index) {
+                                return Expanded(
+                                    child: ClipRRect(
+                                        child: Column(
+                                            children: [
+                                              Text(newsList[index].title),
+                                              Text(newsList[index].date),
+                                              Text(newsList[index].description),
+                                            ]
+                                        )
+                                    )
+                                );
+                              },
+                              itemCount: newsList.length,
+                            )
+                        )
+                      ]
+                  )
+              ),
+            )
+          )
+        ]
+      ),
+    );
+  }
+}
---
+++ lib/news.dart
@@ -0,0 +1,60 @@
+class News {
+  String title;
+  String date;
+  String description;
+
+  News({
+    required this.title,
+    required this.date,
+    required this.description,
+});
+}
+
+var newsList = [
+  News(
+    title: 'Pemberlakuan Perkuliahan Hybrid',
+    date: '15 Juli 2021',
+    description: 'Berdasarkan SK Rektor nomor 23/IX/2021, mulai dari tanggal 23 Agustus 2021, perkuliahan mahasiswa S1 tingkat dua ke atas dilakukan secara hybrid. Keterangan lebih lanjut dapat dilihat webkampus.ac.id',
+  ),
+  News(
+    title: 'Pengumuman Wisuda Kedua Tahun Akademik 2021/2021',
+    date: '10 Juni 2021',
+    description: 'Berdasarkan anjuran SATGAS COVID-19 Kabupaten Bandung dan PPKM, wisuda kedua tahun akademik 2021/2021 akan dilaksanakan secara daring melalui video conference. Wisuda juga dapat disaksikan secara langsung melalui media YouTube.',
+  ),
+  News(
+    title: 'Lorem ipsum',
+    date: '31 Desember 2020',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, purus ac elementum pretium, velit turpis ullamcorper turpis, et fringilla felis urna at mi. Quisque ut semper mi. Curabitur gravida ultrices eros, vel scelerisque enim sagittis sed. Nunc non lacus id arcu viverra tempus. In fermentum mi at purus pellentesque eleifend. Phasellus viverra varius pretium. Vivamus non metus sollicitudin, consectetur lectus id, consectetur dolor.',
+  ),
+  News(
+    title: 'Lorem ipsum',
+    date: '31 Desember 2020',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, purus ac elementum pretium, velit turpis ullamcorper turpis, et fringilla felis urna at mi. Quisque ut semper mi. Curabitur gravida ultrices eros, vel scelerisque enim sagittis sed. Nunc non lacus id arcu viverra tempus. In fermentum mi at purus pellentesque eleifend. Phasellus viverra varius pretium. Vivamus non metus sollicitudin, consectetur lectus id, consectetur dolor.',
+  ),
+  News(
+    title: 'Lorem ipsum',
+    date: '31 Desember 2020',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, purus ac elementum pretium, velit turpis ullamcorper turpis, et fringilla felis urna at mi. Quisque ut semper mi. Curabitur gravida ultrices eros, vel scelerisque enim sagittis sed. Nunc non lacus id arcu viverra tempus. In fermentum mi at purus pellentesque eleifend. Phasellus viverra varius pretium. Vivamus non metus sollicitudin, consectetur lectus id, consectetur dolor.',
+  ),
+  News(
+    title: 'Lorem ipsum',
+    date: '31 Desember 2020',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, purus ac elementum pretium, velit turpis ullamcorper turpis, et fringilla felis urna at mi. Quisque ut semper mi. Curabitur gravida ultrices eros, vel scelerisque enim sagittis sed. Nunc non lacus id arcu viverra tempus. In fermentum mi at purus pellentesque eleifend. Phasellus viverra varius pretium. Vivamus non metus sollicitudin, consectetur lectus id, consectetur dolor.',
+  ),
+  News(
+    title: 'Lorem ipsum',
+    date: '31 Desember 2020',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, purus ac elementum pretium, velit turpis ullamcorper turpis, et fringilla felis urna at mi. Quisque ut semper mi. Curabitur gravida ultrices eros, vel scelerisque enim sagittis sed. Nunc non lacus id arcu viverra tempus. In fermentum mi at purus pellentesque eleifend. Phasellus viverra varius pretium. Vivamus non metus sollicitudin, consectetur lectus id, consectetur dolor.',
+  ),
+  News(
+    title: 'Lorem ipsum',
+    date: '31 Desember 2020',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, purus ac elementum pretium, velit turpis ullamcorper turpis, et fringilla felis urna at mi. Quisque ut semper mi. Curabitur gravida ultrices eros, vel scelerisque enim sagittis sed. Nunc non lacus id arcu viverra tempus. In fermentum mi at purus pellentesque eleifend. Phasellus viverra varius pretium. Vivamus non metus sollicitudin, consectetur lectus id, consectetur dolor.',
+  ),
+  News(
+    title: 'Lorem ipsum',
+    date: '31 Desember 2020',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, purus ac elementum pretium, velit turpis ullamcorper turpis, et fringilla felis urna at mi. Quisque ut semper mi. Curabitur gravida ultrices eros, vel scelerisque enim sagittis sed. Nunc non lacus id arcu viverra tempus. In fermentum mi at purus pellentesque eleifend. Phasellus viverra varius pretium. Vivamus non metus sollicitudin, consectetur lectus id, consectetur dolor.',
+  ),
+];
+
